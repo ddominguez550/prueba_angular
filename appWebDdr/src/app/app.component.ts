a3eb45c5c375f80738d6dcb69c74db7439cfa71f
ddominguez550 <daniel.dominguez550@gmail.com>
2022-09-13 00:03:53 -0500

    ajuste para las peticiones

--- appWebDdr/src/app/app.component.ts
+++ appWebDdr/src/app/app.component.ts
@@ -10,16 +10,14 @@ export class AppComponent implements OnInit{
 
   personas = new Array<any>();
 
-  headers = new HttpHeaders({'Access-Control-Allow-Origin' : '*'})
+  //headers = new HttpHeaders({'Access-Control-Allow-Origin' : '*'})
 
   constructor(public http: HttpClient){}
 
   ngOnInit(): void {
-    this.title='nuevo';
-    console.log("antes");
+    this.title='Personas';
     this.http.get<any>('http://localhost:8080/personas/recuperarTodos').subscribe(response => {
-           this.personas=response.data;//
-           console.log("hola");
+           this.personas=response;//
         })
   }
   title = 'appWebDdr';
